b3a4db95cf2d5d28ce1e09aaccb273ef78810213
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-07-28 16:20:36 -0400

    Creando los modelos.

---
+++ Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarbudosShop.Models
+{
+    public class Categoria
+    {
+
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public ICollection<Producto> Productos { get; set; }
+    }
+}
