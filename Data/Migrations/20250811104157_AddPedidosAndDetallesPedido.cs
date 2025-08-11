f9abfba4d37f5a68395b7348c2d502f2f1449aa6
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-08-11 06:52:55 -0400

    Agregando migraciones

---
+++ Data/Migrations/20250811104157_AddPedidosAndDetallesPedido.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarbudosShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPedidosAndDetallesPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaPedido = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Calle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Sector = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Ciudad = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Provincia = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetallesPedido",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPedido = table.Column<int>(type: "int", nullable: false),
+                    IdProducto = table.Column<int>(type: "int", nullable: false),
+                    ProductoIdProducto = table.Column<int>(type: "int", nullable: true),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesPedido", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedido_Pedidos_IdPedido",
+                        column: x => x.IdPedido,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedido_Productos_ProductoIdProducto",
+                        column: x => x.ProductoIdProducto,
+                        principalTable: "Productos",
+                        principalColumn: "IdProducto");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedido_IdPedido",
+                table: "DetallesPedido",
+                column: "IdPedido");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedido_ProductoIdProducto",
+                table: "DetallesPedido",
+                column: "ProductoIdProducto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetallesPedido");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
